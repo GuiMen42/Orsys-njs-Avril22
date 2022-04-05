1c7217a113b1b31000d6261edce979bc52a8d1f1
Guillaume Menard - Formation Orsys <guillaume.menard@outlook.com>
2022-04-05 13:57:37 +0100

    dev: Hello world

--- Back/server.js
+++ Back/server.js
@@ -1,4 +1,23 @@
 console.log("About to start the server");
-if (2 > 3) {
-  console.log("zef,");
-}
+
+const express = require("express");
+const serveIndex = require("serve-index");
+
+const app = express();
+const port = 3000;
+
+// Tous les types de requetes
+app.use((req, res, next) => {
+  console.log("req: ", req.url);
+  next();
+});
+
+// envoie d'une réponse sur une requête get
+// Ajout * dans le path pour prendre en compte toute les requetes
+app.get("/*", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
