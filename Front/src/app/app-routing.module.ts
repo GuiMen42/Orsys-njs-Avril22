e9c6e6bc84df6763bacba98e0412669d10c0de8f
Guillaume Menard - Formation Orsys <guillaume.menard@outlook.com>
2022-04-07 09:31:46 +0200

    dev: Step 4 - Ajout des liens vers les pages

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
+import { LegalComponent } from './routes/legal/legal.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, //dans le path, il ne faut pas mettre le /
-  { path: 'legal', component: HomeComponent },
+  { path: '', component: HomeComponent }, // Dans le path, il ne faut pas mettre le /
+  { path: 'legal', component: LegalComponent },
 ];
 
 @NgModule({
