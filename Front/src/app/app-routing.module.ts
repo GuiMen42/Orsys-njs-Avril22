534a99783a0274d78af83c496253c810851e0a69
Guillaume Menard - Formation Orsys <guillaume.menard@outlook.com>
2022-04-07 09:19:37 +0200

    dev: Step 2 - Ajout des chemins

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, //dans le path, il ne faut pas mettre le /
+  { path: 'legal', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
