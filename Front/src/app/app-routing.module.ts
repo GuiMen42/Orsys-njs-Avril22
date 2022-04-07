8910b1925616e70c9cacb2d40a2563baba6b8cf3
Guillaume Menard - Formation Orsys <guillaume.menard@outlook.com>
2022-04-07 09:55:41 +0200

    dev: Création du bundle Stock

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {
+  PreloadAllModules,
+  PreloadingStrategy,
+  RouterModule,
+  Routes,
+} from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, // Dans le path, il ne faut pas mettre le /
   { path: 'legal', component: LegalComponent },
+  {
+    path: 'stock',
+    loadChildren: () =>
+      import('./stock/stock.module').then((m) => m.StockModule),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }), // Preloading permet de faire du chargement masqués
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
