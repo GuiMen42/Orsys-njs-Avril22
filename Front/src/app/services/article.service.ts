e4cd6ce83a812c3c561c13f7ff3a028bc63b3991
Guillaume Menard - Formation Orsys <guillaume.menard@outlook.com>
2022-04-07 15:02:08 +0200

    dev: Front - Post

--- Front/src/app/services/article.service.ts
+++ Front/src/app/services/article.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 import { Article } from '../interfaces/article';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
+  public articles: Article[] = [
     { name: 'Pince', price: 3.52, qty: 234 },
     { name: 'Pince', price: 3.52, qty: 234 },
     { name: 'Pince', price: 3.52, qty: 234 },
@@ -22,13 +23,26 @@ export class ArticleService {
     { name: 'Pince', price: 3.52, qty: 234 },
     { name: 'Pince', price: 3.52, qty: 234 },
   ];
-
-  urlServer: string = 'http://localhost:3000/';
-  urlApi: string = 'api/';
-  urlArticles: string = 'articles';
+  public urlApi: string = 'api/';
+  public urlArticles: string = 'articles';
+  public urlServer: string = 'http://localhost:3000/';
 
   constructor(private http: HttpClient) {
     this.retrieveAll();
+  }
+
+  async add(article: Article) {
+    try {
+      await lastValueFrom(
+        this.http.post<void>(
+          this.urlServer + this.urlApi + this.urlArticles,
+          article
+        )
+      );
+    } catch (err) {
+      console.log('err: ', err);
+      throw err;
+    }
   }
 
   retrieveAll() {
--- Front/src/app/stock/add/add.component.ts
+++ Front/src/app/stock/add/add.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Article } from 'src/app/interfaces/article';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  formulaire = new FormGroup({
+    name: new FormControl('truc', [Validators.required]),
+    price: new FormControl('2.78', [Validators.required]),
+    qty: new FormControl('45', [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(
+    private articleService: ArticleService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  async submit() {
+    console.log('submit');
+    try {
+      await this.articleService.add(this.formulaire.value as Article);
+      await this.router.navigate(['..'], { relativeTo: this.route });
+    } catch (err) {
+      console.log('err: ', err);
+    }
   }
-
 }
