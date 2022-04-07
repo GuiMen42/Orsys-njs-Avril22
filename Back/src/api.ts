cf2f38250e01b964fd08ff1c924ba0783407010d
Guillaume Menard - Formation Orsys <guillaume.menard@outlook.com>
2022-04-07 15:23:57 +0200

    dev: Backend - Post Add Article

--- Back/src/api.ts
+++ Back/src/api.ts
@@ -1,4 +1,4 @@
-import { Router } from "express"; // {} permet de récupérer directement un object de la bibliothèque
+import { json, Router } from "express"; // {} permet de récupérer directement un object de la bibliothèque
 import { Article } from "./interfaces/article";
 
 const app = Router();
@@ -8,6 +8,10 @@ const articles: Article[] = [
   { name: "Marteau", price: 20, qty: 250 },
   { name: "autres articles", price: 20, qty: 250 },
 ];
+
+// Middleware
+// Regarde l'entête, si content-type = application/json alors lecture du body et mise du json dans la partie body de la requete recu
+app.use(json());
 
 // Middleware de crash du serveur
 app.get("/crash", (req, res, next) => {
@@ -24,6 +28,20 @@ app.get("/date", (req, res, next) => {
 
 app.get("/articles", (req, res) => {
   res.json(articles);
+});
+
+app.post("/articles", (req, res) => {
+  (async () => {
+    try {
+      const article: Article = req.body;
+      console.log("article: ", article);
+      articles.push(article);
+      res.status(201).json(article);
+    } catch (err) {
+      console.log("err: ", err);
+      res.status(500).end();
+    }
+  })();
 });
 
 export const export_api = app; // Nom de l'objet dans les autres files
