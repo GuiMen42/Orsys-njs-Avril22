a203d94e7b643b7f9218f0e7e0b7dcb5ec162a9b
Guillaume Menard - Formation Orsys <guillaume.menard@outlook.com>
2022-04-08 10:47:29 +0200

    dev: Création du service article avec gestion RAM

--- Back/src/api.ts
+++ Back/src/api.ts
@@ -1,15 +1,11 @@
 import { json, Router } from "express"; // {} permet de récupérer directement un object de la bibliothèque
 import { Article } from "./interfaces/article";
 import morgan, { Morgan } from "morgan";
-import { v4 as uuidv4 } from "uuid";
+import { RAMArticlesService } from "./services/RAMArticles.service";
+
+const articlesService = new RAMArticlesService();
 
 const app = Router();
-
-let articles: Article[] = [
-  { id: "1", name: "Tondeuse", price: 120, qty: 9 },
-  { id: "2", name: "Marteau", price: 20, qty: 250 },
-  { id: "3", name: "autres articles", price: 20, qty: 250 },
-];
 
 // Middleware
 // Regarde l'entête, si content-type = application/json alors lecture du body et mise du json dans la partie body de la requete recu
@@ -33,6 +29,7 @@ app.get("/date", (req, res, next) => {
 app.get("/articles", (req, res) => {
   (async () => {
     try {
+      const articles = await articlesService.retrieveAll();
       res.json(articles);
     } catch (err) {
       console.log("err: ", err);
@@ -45,9 +42,9 @@ app.post("/articles", (req, res) => {
     try {
       const article: Article = req.body;
       console.log("article: ", article);
-      article.id = uuidv4();
-      articles.push(article);
-      res.status(201).json(article);
+      const addedArticle = await articlesService.add(article);
+
+      res.status(201).json(addedArticle);
     } catch (err) {
       console.log("err: ", err);
       res.status(500).end();
@@ -60,7 +57,7 @@ app.delete("/articles", (req, res) => {
     try {
       const ids: string[] = req.body;
       console.log("ids: ", ids);
-      articles = articles.filter((a) => !ids.includes(a.id));
+      await articlesService.remove(ids);
       res.status(204).end();
     } catch (err) {
       console.log("err: ", err);
