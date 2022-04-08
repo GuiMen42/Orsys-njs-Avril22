d3a614eaee2c4a3efce5189b5d216b13dbd108b2
Guillaume Menard - Formation Orsys <guillaume.menard@outlook.com>
2022-04-08 14:15:58 +0200

    dev: Observable : debounce

--- Back/src/services/FileArticle.service.ts
+++ Back/src/services/FileArticle.service.ts
@@ -1,46 +1,54 @@
 import { Article } from "../interfaces/article";
 import { v4 as uuidv4 } from "uuid";
 import fs from "fs";
+import { BehaviorSubject, debounceTime } from "rxjs";
 
-let articles: Article[] = [];
+//let articles: Article[] = [];
+
+// Régle de nommage $ à la fin pour dire que c'est un observable
+const articles$ = new BehaviorSubject<Article[]>([]);
 
 const JSON_FILE = "data/articles.json";
 const init = () => {
   try {
     const str = fs.readFileSync(JSON_FILE, { encoding: "utf-8" });
-    articles = JSON.parse(str);
+    articles$.next(JSON.parse(str));
   } catch (err) {
     console.log("err: ", err);
-    articles = [
+    articles$.next([
       { id: "1", name: "Tondeuse", price: 120, qty: 9 },
       { id: "2", name: "Marteau", price: 20, qty: 250 },
       { id: "3", name: "autres articles", price: 20, qty: 250 },
-    ];
+    ]);
   }
+
+  // pipe(debounceTime(2000)) // permet d'exécuter la fonction au bout de 2 secondes
+  articles$.pipe(debounceTime(2000)).subscribe({
+    next: (articles) => {
+      fs.promises.writeFile(JSON_FILE, JSON.stringify(articles, null, 2)); // null, 2) mise en forme du fichietr json
+    },
+  });
 };
 
 // Fonction appelé une seule fois et au premier import de la bibliothèque
 init();
 
-const save = async () => {
-  fs.promises.writeFile(JSON_FILE, JSON.stringify(articles));
-};
-
 export class FileArticleService {
   async add(article: Article): Promise<Article> {
     const addedArticle = { ...article };
     addedArticle.id = uuidv4();
-    articles.push(addedArticle);
-    await save();
+    articles$.next([...articles$.value, addedArticle]);
     return addedArticle;
   }
 
   async remove(ids: string[]) {
-    articles = articles.filter((a) => !ids.includes(a.id));
-    await save();
+    const remainingArticles = articles$.value.filter(
+      (a) => !ids.includes(a.id)
+    );
+    articles$.next(remainingArticles);
   }
 
   async retrieveAll() {
-    return articles;
+    return articles$.value;
   }
 }
